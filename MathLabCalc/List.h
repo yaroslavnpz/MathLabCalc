e5ea8eecc56afc13439ccfaad2419285f6cebf57
yaroslavnzp <yaroslavnzp@yandex.ru>
2023-02-04 19:34:43 +0500

    Create Seg, Quad and List

---
+++ MathLabCalc/List.h
@@ -0,0 +1,66 @@
+#pragma once
+#include "BaseMathTypes.h"
+
+
+#include <vector>
+
+
+namespace calc {
+
+
+class List {
+   private:
+    std::vector<MathType> data;
+
+
+    void optimize();
+
+   public:
+    List() = default;
+    List(const List&) = default;
+    List(List&&) noexcept = default;
+    ~List() noexcept = default;
+
+
+    List(const std::initializer_list<MathType>& val);
+    List(const MathType& val);
+
+
+    List& operator=(const List&) = default;
+    List& operator=(List&&) noexcept = default;
+
+
+    const List operator+() const;
+    const List operator-() const;
+
+    const List operator+(const List& val) const;
+    const List operator-(const List& val) const;
+    const List operator*(const List& val) const;
+    const List operator/(const List& val) const;
+    const List operator%(const List& val) const;
+
+
+    const List intDiv(const List& val) const;
+
+
+    const List operator&(const List& val) const;
+    const List operator|(const List& val) const;
+    const List operator^(const List& val) const;
+    const List operator<<(const List& val) const;
+    const List operator>>(const List& val) const;
+
+
+    const List operator||(const List& val) const;
+    const List operator&&(const List& val) const;
+
+
+    const bool operator==(const List& val) const;
+    const bool operator!=(const List& val) const;
+    const bool operator<(const List& val) const;
+    const bool operator<=(const List& val) const;
+    const bool operator>(const List& val) const;
+    const bool operator>=(const List& val) const;
+};
+
+
+}  // namespace calc
---
+++ MathLabCalc/Seg.h
@@ -0,0 +1,119 @@
+#pragma once
+#include "BaseMathTypes.h"
+
+
+#include <array>
+
+
+namespace calc {
+
+
+class Seg {
+   private:
+    std::array<MathType, 2> data;
+
+
+    void optimize();
+
+   public:
+    Seg() = default;
+    Seg(const Seg&) = default;
+    Seg(Seg&&) noexcept = default;
+    ~Seg() noexcept = default;
+
+
+    Seg(const MathType& x, const MathType& y);
+
+
+    Seg& operator=(const Seg&) = default;
+    Seg& operator=(Seg&&) noexcept = default;
+
+
+    const Seg operator+() const;
+    const Seg operator-() const;
+
+    const Seg operator+(const Seg& val) const;
+    const Seg operator-(const Seg& val) const;
+    const Seg operator*(const Seg& val) const;
+    const Seg operator/(const Seg& val) const;
+    const Seg operator%(const Seg& val) const;
+
+
+    const Seg intDiv(const Seg& val) const;
+
+
+    const Seg operator&(const Seg& val) const;
+    const Seg operator|(const Seg& val) const;
+    const Seg operator^(const Seg& val) const;
+    const Seg operator<<(const Seg& val) const;
+    const Seg operator>>(const Seg& val) const;
+
+
+    const Seg operator||(const Seg& val) const;
+    const Seg operator&&(const Seg& val) const;
+
+
+    const bool operator==(const Seg& val) const;
+    const bool operator!=(const Seg& val) const;
+    const bool operator<(const Seg& val) const;
+    const bool operator<=(const Seg& val) const;
+    const bool operator>(const Seg& val) const;
+    const bool operator>=(const Seg& val) const;
+};
+
+
+class Quad {
+   private:
+    std::array<MathType, 4> data;
+
+
+    void optimize();
+
+   public:
+    Quad() = default;
+    Quad(const Quad&) = default;
+    Quad(Quad&&) noexcept = default;
+    ~Quad() noexcept = default;
+
+
+    Quad(const MathType& x, const MathType& y, const MathType& x1, const MathType& y1);
+
+
+    Quad& operator=(const Quad&) = default;
+    Quad& operator=(Quad&&) noexcept = default;
+
+
+    const Quad operator+() const;
+    const Quad operator-() const;
+
+    const Quad operator+(const Quad& val) const;
+    const Quad operator-(const Quad& val) const;
+    const Quad operator*(const Quad& val) const;
+    const Quad operator/(const Quad& val) const;
+    const Quad operator%(const Quad& val) const;
+
+
+    const Quad intDiv(const Quad& val) const;
+
+
+    const Quad operator&(const Quad& val) const;
+    const Quad operator|(const Quad& val) const;
+    const Quad operator^(const Quad& val) const;
+    const Quad operator<<(const Quad& val) const;
+    const Quad operator>>(const Quad& val) const;
+
+
+    const Quad operator||(const Quad& val) const;
+    const Quad operator&&(const Quad& val) const;
+
+
+    const bool operator==(const Quad& val) const;
+    const bool operator!=(const Quad& val) const;
+    const bool operator<(const Quad& val) const;
+    const bool operator<=(const Quad& val) const;
+    const bool operator>(const Quad& val) const;
+    const bool operator>=(const Quad& val) const;
+};
+
+
+}  // namespace calc
